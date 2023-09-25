723f7fe84c2e0282e4040658a80429858df38ca1
Juan Torres <junete190@gmail.com>
2023-09-24 21:22:34 -0500

    Final clase 2

--- clase1.1.1/src/App.jsx
+++ clase1.1.1/src/App.jsx
@@ -1,32 +1,50 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   //Parte lógica
 
+  const personal = {
+    title: "Personal",
+    nombre: "Juan Torres",
+    age: "24 años",
+    movie: "Gran Turismo",
+    music: "Salsa",
+  };
+
   const handleClick = () => {
-    console.log('Juan Diego Torres Escárraga')
-  }
+    console.log("Juan Diego Torres Escárraga");
+  };
 
   //TODO Mostrar una alerta al dar click en cualquier parte del contenedor de la información.
 
   const handleCard = () => {
-    alert(document.querySelector('.card>h2').innerHTML)
-  }
+    alert(document.querySelector(".card>h2").innerHTML);
+  };
 
   //Parte visual
   return (
     <>
-      <article className='card' onClick={handleCard}>
-        <h2>Personal</h2>
+      <article className="card" onClick={handleCard}>
+        <h2>{personal.title}</h2>
         <ul>
-          <li onClick={handleClick}><b>Nombre:</b> Juan Torres</li>
-          <li><b>Edad:</b> 24 años</li>
-          <li><b>Película:</b> Gran Turismo</li>
-          <li><b>Música favorita:</b> Salsa</li>
+          <li onClick={handleClick}>
+            <b>Nombre: </b>
+            {personal.nombre}
+          </li>
+          <li>
+            <b>Edad: </b> {personal.age}
+          </li>
+          <li>
+            <b>Película: </b> {personal.movie}
+          </li>
+          <li>
+            <b>Música favorita :</b>
+            {personal.music}
+          </li>
         </ul>
-      </article>    
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
